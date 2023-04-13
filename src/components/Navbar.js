c3807a3ae5640cf51d788a4154426ca4b826b1a3
YarosThk <yaroslavtashak@gmail.com>
2023-04-13 18:41:28 +0200

    Removed unused NavLink

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,20 @@
-import { NavbarWrapper, NavigationWrapper, NavbarLogo, StyledNavLink } from "./styled/Navbar.styled";
-import { NavLink } from "react-router-dom";
-import logo from "../images/nav_logo.png"
+import {
+  NavbarWrapper,
+  NavigationWrapper,
+  NavbarLogo,
+  StyledNavLink,
+} from "./styled/Navbar.styled";
+import logo from "../images/nav_logo.png";
 
 export const Navbar = () => {
-
-    return (
-        <NavbarWrapper>
-            <NavbarLogo src={logo}/>
-            <NavigationWrapper>
-                <StyledNavLink to="/">Home</StyledNavLink>
-                <StyledNavLink to="characters" >Characters</StyledNavLink>
-                {/* <StyledNavLink to="favorites">Favorites</StyledNavLink> */}
-                <StyledNavLink to="about">About</StyledNavLink>
-            </NavigationWrapper>
-        </NavbarWrapper>
-    ) 
-}
+  return (
+    <NavbarWrapper>
+      <NavbarLogo src={logo} />
+      <NavigationWrapper>
+        <StyledNavLink to="/">Home</StyledNavLink>
+        <StyledNavLink to="characters">Characters</StyledNavLink>
+        <StyledNavLink to="about">About</StyledNavLink>
+      </NavigationWrapper>
+    </NavbarWrapper>
+  );
+};
